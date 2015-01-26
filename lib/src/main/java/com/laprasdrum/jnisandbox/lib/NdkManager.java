2df8b2a56df5a835e0b9e1c577412822ad036ee9
laprasDrum <laprasdrum@yuyacat.com>
2015-01-26 18:36:58 +0900

    Fixed comments

--- lib/src/main/java/com/laprasdrum/jnisandbox/lib/NdkManager.java
+++ lib/src/main/java/com/laprasdrum/jnisandbox/lib/NdkManager.java
@@ -15,14 +15,14 @@ public class NdkManager {
     }
 
     /* A native method that is implemented by the
-     * 'hello-jni' native library, which is packaged
+     * 'JniSandbox' native library, which is packaged
      * with this application.
      */
     public native String stringFromJNI();
     public native int addTwoFromJNI(int a, int b);
     public native int countFromJNI();
 
-    /* this is used to load the 'hello-jni' library on application
+    /* this is used to load the 'JniSandbox' library on application
      * startup. The library has already been unpacked into
      * /data/data/com.laprasdrum.jnisandbox/lib/libhello-jni.so at
      * installation time by the package manager.
